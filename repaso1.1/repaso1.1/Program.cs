aadb609589792be841e1f120629db11a4a597d9e
frlopez88 <68876977+frlopez88@users.noreply.github.com>
2023-04-18 20:20:31 -0600

    Suma de Numeros

--- repaso1.1/repaso1.1/Program.cs
+++ repaso1.1/repaso1.1/Program.cs
@@ -1,3 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+// Programa que suma dos numeros
+int a = 0;
+int b = 0;
+
+Console.WriteLine("Ingrese el primer numero");
+a = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el segundo numero");
+b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("La suma de los numero es " + (a + b));
