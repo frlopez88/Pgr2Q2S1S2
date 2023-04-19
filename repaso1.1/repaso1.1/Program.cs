cee700ff5d59db70c6534c18efa4b852e4155fdf
frlopez88 <68876977+frlopez88@users.noreply.github.com>
2023-04-18 20:35:32 -0600

    Commit Tipos de Datos

--- repaso1.1/repaso1.1/Program.cs
+++ repaso1.1/repaso1.1/Program.cs
@@ -10,3 +10,18 @@ Console.WriteLine("Ingrese el segundo numero");
 b = int.Parse(Console.ReadLine());
 
 Console.WriteLine("La suma de los numero es " + (a + b));
+
+// int, double, float, bool, string, char
+
+int a1 = 20;
+double d1 = 20.00;
+float f1 = 20.0f;
+bool b1 = true;
+string s1 = "Hola";
+char c1 = 'c';
+
+
+string cad1 = "20";
+int conversion = int.Parse(cad1);
+
+Console.WriteLine (conversion + 5);
